f0d59aaf984ce67857920b50d17292d76e7482f4
Orpheus424 <68345234+Orpheus424@users.noreply.github.com>
2021-08-28 20:32:54 +0300

    Add smooth skeletal animations

---
+++ Assets/Scripts/GhostAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostAnimation : MonoBehaviour
+{
+    public float _idlespeed;
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + _idlespeed / 1000, transform.position.z);
+    }
+}
